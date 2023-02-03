e9daa50f738e19c2111e65410a46e95f1f62c9dc
김진영 <jinyoung01099@gmail.com>
2023-02-03 19:00:38 +0900

    [Add] MeetupDetail 페이지 기본 레이아웃 구현

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,16 @@
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <>
+      <MeetupDetail
+        image="https://images.unsplash.com/photo-1517154421773-0529f29ea451?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+        title="First Meetup"
+        address="경기도 안양시 동안구 비산 2동"
+        description="A First Meetup"
+      />
+    </>
+  );
+};
+
+export default MeetupDetails;

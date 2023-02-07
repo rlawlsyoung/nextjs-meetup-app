f5f5caadf61be1ee6f76eb8f944ab892781be433
김진영 <jinyoung01099@gmail.com>
2023-02-07 19:10:13 +0900

    [Add] MongoDB 연결

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,27 @@
+import { MongoClient } from "mongodb";
+
 // /api/new-meetup
 
-const handler = (req, res) => {
+const handler = async (req, res) => {
   if (req.method === "POST") {
     const data = req.body;
 
     const { title, image, address, description } = data;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://jinyoung985:bC4VncbF2Smnd0tc@cluster0.e6y5t4f.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne();
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 };
 
--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ const DUMMY_MEETUPS = [
     description: "This is a first meetup",
   },
   {
-    id: "m1",
+    id: "m2",
     title: "A Second Meetup",
     image:
       "https://images.unsplash.com/photo-1517154421773-0529f29ea451?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",

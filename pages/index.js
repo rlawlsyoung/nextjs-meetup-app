b3cc4dd0a2f4ceea435edab9232b4265056fbd48
김진영 <jinyoung01099@gmail.com>
2023-02-11 19:47:51 +0900

    [Add] head 메타 데이터 추가

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,21 @@
+import Head from "next/head";
 import { MongoClient } from "mongodb";
 
 import MeetupList from "../components/meetups/MeetupList";
 
 const HomePage = (props) => {
-  return <MeetupList meetups={props.meetups} />;
+  return (
+    <>
+      <Head>
+        <title>React Meetups</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </>
+  );
 };
 
 // export const getServerSideProps = async (context) => {
